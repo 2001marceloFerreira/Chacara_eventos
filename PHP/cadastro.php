b89684d3d02dfab60d45b55ba15415c9ef313d61
Marcelo Ferreira <marcelopmras@gmail.com>
2022-05-31 22:58:32 -0300

    commiit

--- PHP/cadastro.php
+++ PHP/cadastro.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistema de Cadastro - PHP + MySQL - Canal TI</title>
+    <title>Cadastro chacara</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="../css/bulma.min.css" />
     <link rel="stylesheet" type="text/css" href="../css/login.css">
@@ -19,8 +19,8 @@ session_start();
         <div class="hero-body">
             <div class="container has-text-centered">
                 <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-grey">Sistema de Cadastro</h3>
-                    <h3 class="title has-text-grey"><a href="https://youtube.com/canaltioficial" target="_blank">Canal TI</a></h3>
+                    <h3 class="title has-text-grey">Cadastro chácara</h3>
+                    <h3 class="title has-text-grey"><a href="../index.html" target="_blank">Revoada</a></h3>
                     <?php
                     if (isset($_SESSION['status_cadastro'])) :
                     ?>
